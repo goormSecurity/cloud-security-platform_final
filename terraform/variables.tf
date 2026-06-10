599423dc6ef7066290fa05099eff14cc1d7cd67a
Yoo Jiwon <iamwldnjs07@gmail.com>
2026-06-10 23:38:43 +0900

    chore: Terraform 기본 구조 설정

---
+++ terraform/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS 리전"
+  type        = string
+  default     = "ap-northeast-2"
+}
+
+variable "environment" {
+  description = "환경 구분"
+  type        = string
+  default     = "dev"
+}
+
+variable "project_name" {
+  description = "프로젝트명"
+  type        = string
+  default     = "cloud-sec"
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+  default     = "10.0.0.0/16"
+}
